447ab08987c5c5d6f10ae495318ee982c49df320
shaliko96 <shalikohayrapetyan96@gmail.com>
2023-01-20 11:30:15 +0400

    renderbuilturl

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import checker from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
+  experimental: {
+    renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string, hostType: 'js' | 'css' | 'html', type: 'public' | 'asset' }) {
+      return 'https://effulgent-sherbet-6b8472.netlify.app/'
+    }
+  }
 });
